4b8eb55d08b1d8b67973e0bb95222a58fbd1b858
Diego Bassan <bsn.diego@gmail.com>
2018-03-06 16:58:44 -0300

    Validações de Tipos de Usuários implementadas.
    Signed-off-by: Diego Bassan <bsn.diego@gmail.com>

--- app/models/user_type.rb
+++ app/models/user_type.rb
@@ -1,2 +1,6 @@
 class UserType < ActiveRecord::Base
+    validates_presence_of :name
+    validates_presence_of :description
+    validates_uniqueness_of :name, case_sensitive: false
+    validates_numericality_of :commissionPercentage, allow_nil: true
 end
